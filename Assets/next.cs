fff6596249c90626c4c63f44756675103552eb20
Haoyu Li <1693044@CEGEPGIM.LCL>
2017-08-29 12:05:51 -0400

    Second commit

---
+++ Assets/next.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class next : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("gamemode", LoadSceneMode.Single);
+    }
+}
